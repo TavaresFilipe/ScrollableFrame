1e27f3acfc4033b96d53d988aa6a4b441d9ae3e0
Filipe Tavares <filipctavares@gmail.com>
2021-09-30 11:26:54 -0300

    2nd commit: improving ingeritance use

--- main.py
+++ main.py
@@ -3,33 +3,29 @@ from tkinter import ttk
 
 
 class ScrollableFrame(tk.Frame):
-    def __init__(self, parent=None, **kargs):
-
+    def __init__(self, parent=None, **kwargs):
         # Create an external Frame
         self.master_frame = tk.Frame(parent)
-        self.master_frame.pack(fill='both', expand=1)
+        self.master_frame.grid_rowconfigure(0, weight=1)
+        self.master_frame.grid_columnconfigure(0, weight=1)
 
         # Create a Canvas
-        self.canvas = tk.Canvas(self.master_frame)
+        self.canvas = tk.Canvas(self.master_frame, **kwargs)
 
-        super().__init__(self.canvas, kargs)
+        super().__init__(self.canvas, bg=self.canvas["bg"])
 
         # Add a Scrollbar to the Canvas
         self.scrollbar = tk.Scrollbar(self.master_frame, orient='vertical', command=self.canvas.yview)
-        self.scrollbar.pack(side='right', fill='y')
+        self.scrollbar.grid(row=0, column=1, sticky="news")
 
         # Configure the Canvas
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")), add=True)
         self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
 
-        # self.parent = self.canvas
-
-        # self.internal_frame = tk.Frame(self.canvas)
-
         # Create another frame INSIDE the Canvas
         self.canvas.create_window((0, 0), window=self, anchor="nw")
-        self.canvas.pack(side='left', fill='both', expand=1)
+        self.canvas.grid(row=0, column=0, sticky="news")
 
         self.pack = self.master_frame.pack
         self.grid = self.master_frame.grid
@@ -40,13 +36,14 @@ class ScrollableFrame(tk.Frame):
 
     def on_mouse_wheel(self, event):
         # -1 is for direction inversion
-        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
 
 root = tk.Tk()
 root.geometry("500x400")
 
-main_frame = ScrollableFrame(root)
+main_frame = ScrollableFrame(root, bg='black', bd=1, highlightcolor='lightgreen', highlightbackground='lightgreen',
+                             highlightthickness=2)
 main_frame.pack(fill='both', expand=1)
 
 for thing in range(100):
