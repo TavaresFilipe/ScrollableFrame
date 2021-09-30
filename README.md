292d2fa07dd162d4509ca2fae456c5680166dcf5
Filipe Tavares <filipctavares@gmail.com>
2021-09-30 11:44:13 -0300

    4th commit: bind Shift-MouseWheel to horizontal scroll implemented. Now it is possible to hide or show the scrollbars (but bind commands are always active)

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This is a class inherited from tkinter's Frame class with the ability to be scro
 
 ### References:
 
- - https://stackoverflow.com/questions/68229975/tkinter-scrollable-frame-inside-function
- - https://www.youtube.com/watch?v=0WafQCaok6g&t=133s&ab_channel=Codemy.com
- - https://www.youtube.com/watch?v=gRjuZrD-CwU&t=208s&ab_channel=PyTechTamil
- - https://www.youtube.com/watch?v=VmlgrrXAqb4&t=2s&ab_channel=CodeWorked
+- https://stackoverflow.com/questions/66213754/unable-to-scroll-frame-using-mouse-wheel-adding-horizontal-scrollbar/66215091#66215091
+- https://stackoverflow.com/questions/68229975/tkinter-scrollable-frame-inside-function
+- https://stackoverflow.com/questions/31254729/tkinter-bind-shift-mousewheel-to-horizontal-scroll
+- https://www.youtube.com/watch?v=0WafQCaok6g&t=133s&ab_channel=Codemy.com
+- https://www.youtube.com/watch?v=gRjuZrD-CwU&t=208s&ab_channel=PyTechTamil
+- https://www.youtube.com/watch?v=VmlgrrXAqb4&t=2s&ab_channel=CodeWorked
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from tkinter import ttk
 
 
 class ScrollableFrame(tk.Frame):
-    def __init__(self, parent=None, **kwargs):
-        # Create an external Frame
+    def __init__(self, parent=None, hscroll:bool=False, vscroll:bool=False, scrollbar_kwargs={}, **kwargs):
+
+        # Create a master Frame
         self.master_frame = tk.Frame(parent)
         self.master_frame.grid_rowconfigure(0, weight=1)
         self.master_frame.grid_columnconfigure(0, weight=1)
@@ -14,14 +15,26 @@ class ScrollableFrame(tk.Frame):
 
         super().__init__(self.canvas, bg=self.canvas["bg"])
 
-        # Add a Scrollbar to the Canvas
-        self.scrollbar = tk.Scrollbar(self.master_frame, orient='vertical', command=self.canvas.yview)
-        self.scrollbar.grid(row=0, column=1, sticky="news")
+        # Create the 2 scrollbars
+        if vscroll:
+            self.v_scrollbar = tk.Scrollbar(self.master_frame,
+                                            orient="vertical",
+                                            command=self.canvas.yview,
+                                            **scrollbar_kwargs)
+            self.v_scrollbar.grid(row=0, column=1, sticky="news")
+            self.canvas.configure(yscrollcommand=self.v_scrollbar.set)
+        if hscroll:
+            self.h_scrollbar = tk.Scrollbar(self.master_frame,
+                                            orient="horizontal",
+                                            command=self.canvas.xview,
+                                            **scrollbar_kwargs)
+            self.h_scrollbar.grid(row=1, column=0, sticky="news")
+            self.canvas.configure(xscrollcommand=self.h_scrollbar.set)
 
-        # Configure the Canvas
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        # Configure the Canvas bind
         self.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")), add=True)
         self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
+        self.canvas.bind_all('<Shift-MouseWheel>', self.scroll_horizontally)
 
         # Create another frame INSIDE the Canvas
         self.canvas.create_window((0, 0), window=self, anchor="nw")
@@ -38,12 +51,15 @@ class ScrollableFrame(tk.Frame):
         # -1 is for direction inversion
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
+    def scroll_horizontally(self, event):
+        self.canvas.xview_scroll(int(event.delta / 120), "units")
+
 
 root = tk.Tk()
 root.geometry("500x400")
 
 main_frame = ScrollableFrame(root, bg='black', bd=1, highlightcolor='lightgreen', highlightbackground='lightgreen',
-                             highlightthickness=2)
+                             highlightthickness=2, vscroll=True, hscroll=True)
 main_frame.pack(fill='both', expand=1)
 
 for thing in range(100):
